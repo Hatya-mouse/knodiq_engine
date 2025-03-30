5ce155578aeebdb2b36fbb95acda0caee3e05c4f
Shuntaro Kasatani <haytanezu@gmail.com>
2025-03-30 10:13:58 +0900

    Multithread playing process

--- src/audio_engine/buffer_player.rs
+++ src/audio_engine/buffer_player.rs
@@ -1,7 +1,7 @@
 use crate::audio_engine::audio_buffer::SAudioBuffer;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::SampleRate;
-use std::sync::{mpsc, Arc};
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -13,10 +13,11 @@ pub enum AudioCommand {
 
 pub struct SAudioBufferPlayer {
     pub buffer: Option<Arc<SAudioBuffer>>,
+    pub is_playing: Arc<Mutex<bool>>,
+    pub frame_index: Arc<Mutex<usize>>,
     stream: Option<cpal::Stream>,
     sender: mpsc::Sender<AudioCommand>,
     receiver: mpsc::Receiver<AudioCommand>,
-    handle: Option<thread::JoinHandle<()>>,
 }
 
 impl SAudioBufferPlayer {
@@ -24,10 +25,11 @@ impl SAudioBufferPlayer {
         let (tx, rx) = mpsc::channel();
         Self {
             buffer: None,
+            is_playing: Arc::new(Mutex::new(false)),
+            frame_index: Arc::new(Mutex::new(0)),
             stream: None,
             sender: tx,
             receiver: rx,
-            handle: None,
         }
     }
 
@@ -62,58 +64,75 @@ impl SAudioBufferPlayer {
         let owned_data = buffer.data.to_owned();
 
         // Spawn a thread to wait for the playback to finish
-        let handle = thread::spawn(move || -> Result<(), Box<dyn std::error::Error>> {
-            let (tx, rx) = mpsc::channel();
+        thread::spawn(move || {
+            let (tx, _) = mpsc::channel();
 
-            let mut frame_index = 0;
+            let is_playing_clone = Arc::clone(&self.is_playing);
+            let frame_index_clone = Arc::clone(&self.frame_index);
 
             // Build the output stream...
-            let stream = device.build_output_stream(
+            let stream = match device.build_output_stream(
                 &stream_config,
                 move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-                    for frame_slice in data.chunks_mut(channels) {
-                        let mut channel_index = 0;
-                        for sample in frame_slice.iter_mut() {
-                            if frame_index < frames {
-                                *sample = owned_data[channel_index][frame_index] * volume_factor;
-                            } else {
-                                *sample = 0.0;
-                                let _ = tx.send(());
+                    let is_playing = is_playing_clone.lock().unwrap();
+                    let mut frame_index = frame_index_clone.lock().unwrap();
+                    if *is_playing {
+                        for frame_slice in data.chunks_mut(channels) {
+                            let mut channel_index = 0;
+                            for sample in frame_slice.iter_mut() {
+                                if *frame_index < frames {
+                                    *sample =
+                                        owned_data[channel_index][*frame_index] * volume_factor;
+                                } else {
+                                    *sample = 0.0;
+                                    let _ = tx.send(());
+                                }
+                                channel_index += 1;
                             }
-                            channel_index += 1;
+                            *frame_index += 1;
                         }
-                        frame_index += 1;
                     }
                 },
                 move |err| eprintln!("Error occurred during building playback stream: {:?}", err),
                 Some(Duration::from_secs(10)),
-            )?;
+            ) {
+                Ok(stream) => stream,
+                Err(err) => {
+                    eprintln!("Error occurred during building playback stream: {:?}", err);
+                    return;
+                }
+            };
 
             // ...and play the stream!
-            stream.play()?;
-
-            while rx.try_recv().is_err() {
-                match receiver.recv() {
-                    Ok(AudioCommand::Play) => {
-                        if let Err(e) = self.play() {
-                            eprintln!("Error while playing: {}", e);
-                        }
-                    }
-                    Ok(AudioCommand::Pause) => {
-                        println!("Paused");
-                        stream.pause();
-                    }
-                    Ok(AudioCommand::Stop) | Err(_) => {
-                        println!("Stopping playback");
-                        stream.pause();
-                        break;
-                    }
+            match stream.play() {
+                Ok(_) => (),
+                Err(err) => {
+                    eprintln!("Error occurred during playing stream: {:?}", err);
+                    return;
                 }
             }
-
-            Ok(())
         });
 
-        Ok(())
+        loop {
+            match self.receiver.recv() {
+                Ok(AudioCommand::Play) => {
+                    if let Err(e) = self.play() {
+                        eprintln!("Error while playing: {}", e);
+                    }
+                }
+                Ok(AudioCommand::Pause) => {
+                    println!("Paused");
+                    let mut is_playing = self.is_playing.lock().unwrap();
+                    *is_playing = false;
+                }
+                Ok(AudioCommand::Stop) | Err(_) => {
+                    println!("Stopping playback");
+                    let mut is_playing = self.is_playing.lock().unwrap();
+                    *is_playing = false;
+                    let mut frame_index = self.frame_index.lock().unwrap();
+                    *frame_index = 0;
+                }
+            }
+        }
     }
 }
