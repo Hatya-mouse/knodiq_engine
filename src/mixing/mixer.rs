4bc859ed6c9c5f93e7a2035610446952af1ef22e
Shuntaro Kasatani <haytanezu@gmail.com>
2025-06-02 20:55:35 +0900

    Bump version to 0.2.41 and add debug output for current playhead in Mixer

--- src/mixing/mixer.rs
+++ src/mixing/mixer.rs
@@ -117,6 +117,8 @@ impl Mixer {
 
             // Increment the playhead duration
             self.playhead_beats += CHUNK_BEATS;
+
+            println!("Current playhead: {:.2} beats", self.playhead_beats);
         }
 
         // Return the mixed output.
