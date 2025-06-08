58c466af29a55db0428bad03c0cd42a5fdd74ca8
Shuntaro Kasatani <haytanezu@gmail.com>
2025-06-08 10:23:51 +0900

    Remove redundant Clone implementation for Value enum

--- src/graph/value.rs
+++ src/graph/value.rs
@@ -1,17 +1,9 @@
 use crate::{AudioBuffer, Sample};
 
+#[derive(Debug, Clone, PartialEq)]
 pub enum Value {
     /// Represents a floating-point value.
     Float(Sample),
     /// Represents a multiple audio samples.
     Buffer(AudioBuffer),
-}
-
-impl Clone for Value {
-    fn clone(&self) -> Self {
-        match self {
-            Value::Float(f) => Value::Float(*f),
-            Value::Buffer(b) => Value::Buffer(b.clone()),
-        }
-    }
 }
