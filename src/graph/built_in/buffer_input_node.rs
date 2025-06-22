345574e0cddd2b0c2720c3b12e47de7bcbf73b59
Shuntaro Kasatani <haytanezu@gmail.com>
2025-06-22 09:18:00 +0900

    Add BufferInputNode and BufferOutputNode implementations

---
+++ src/graph/built_in/buffer_input_node.rs
@@ -0,0 +1,107 @@
+// buffer_input_node.rs
+// A graph node that just pass the audio source.
+// © 2025 Shuntaro Kasatani
+
+use crate::{Node, NodeId, Value};
+use std::any::Any;
+use std::collections::HashMap;
+
+/// A node that does nothing.
+pub struct BufferInputNode {
+    id: NodeId,
+    input: Option<Value>,
+    output: Option<Value>,
+}
+
+impl BufferInputNode {
+    /// Creates a new instance of the BufferInputNode.
+    pub fn new() -> Self {
+        BufferInputNode {
+            id: NodeId::new_v4(),
+            input: None,
+            output: None,
+        }
+    }
+}
+
+impl Node for BufferInputNode {
+    fn process(
+        &mut self,
+        _sample_rate: usize,
+        channels: usize,
+        chunk_start: usize,
+        chunk_end: usize,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let buffer = match self.input.as_ref() {
+            Some(Value::Buffer(buffer)) => Value::Buffer(buffer.clone()),
+            Some(value) => value.clone(),
+            None => Value::Buffer(vec![vec![0.0; chunk_end - chunk_start]; channels]),
+        };
+
+        let mut result = HashMap::new();
+        result.insert("output".to_string(), buffer.clone());
+        self.output = Some(buffer);
+
+        Ok(())
+    }
+
+    fn prepare(&mut self, _: usize) {}
+
+    fn get_input_list(&self) -> Vec<String> {
+        vec!["input".to_string()]
+    }
+
+    fn get_output_list(&self) -> Vec<String> {
+        vec!["buffer".to_string()]
+    }
+
+    fn get_input(&self, property: &str) -> Option<Value> {
+        match property {
+            "input" => match self.input {
+                Some(ref input) => Some(input.clone()),
+                None => None,
+            },
+            _ => None,
+        }
+    }
+
+    fn set_input(&mut self, property: &str, value: Value) {
+        match property {
+            "input" => self.input = Some(value),
+            _ => (),
+        }
+    }
+
+    fn get_output(&self, output: &str) -> Option<Value> {
+        match output {
+            "output" => self.output.clone(),
+            _ => None,
+        }
+    }
+
+    fn get_type(&self) -> String {
+        "BufferInputNode".to_string()
+    }
+
+    fn set_id(&mut self, id: crate::NodeId) {
+        self.id = id;
+    }
+
+    fn get_id(&self) -> NodeId {
+        self.id
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Clone for BufferInputNode {
+    fn clone(&self) -> Self {
+        BufferInputNode {
+            id: self.id.clone(),
+            input: self.input.clone(),
+            output: self.output.clone(),
+        }
+    }
+}
--- src/graph/built_in/mod.rs
+++ src/graph/built_in/mod.rs
@@ -1,6 +1,10 @@
 // audio_engine/graph/built_in/mod.rs
 // © 2025 Shuntaro Kasatani
 
+pub mod buffer_input_node;
+pub mod buffer_output_node;
 pub mod empty_node;
 
+pub use buffer_input_node::BufferInputNode;
+pub use buffer_output_node::BufferOutputNode;
 pub use empty_node::EmptyNode;
