efa2dea8f877fd824a891d6434d8e912ceb69558
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-03-24 02:40:11 +0900

    Implement NoteTrack

--- src/data_types/voice.rs
+++ src/data_types/voice.rs
@@ -1,7 +1,30 @@
 #[repr(C)]
+#[derive(Clone)]
 pub struct Voice {
     pub frequency: f32,
     pub velocity: f32,
     pub is_active: bool,
     pub elapsed_samples: i32,
+}
+
+impl Default for Voice {
+    fn default() -> Self {
+        Self {
+            frequency: 0.0,
+            velocity: 0.0,
+            is_active: false,
+            elapsed_samples: 0,
+        }
+    }
+}
+
+impl Voice {
+    pub fn new(frequency: f32, velocity: f32, is_active: bool, elapsed_samples: i32) -> Self {
+        Self {
+            frequency,
+            velocity,
+            is_active,
+            elapsed_samples,
+        }
+    }
 }
--- src/track/note_track/mod.rs
+++ src/track/note_track/mod.rs
@@ -4,11 +4,11 @@ mod voice_event;
 pub use note_region::{Note, NoteRegion};
 
 use crate::{
-    data_types::{AudioContext, Beats},
+    data_types::{AudioContext, Beats, Voice},
     graph::{Graph, error::GraphError},
     track::{RegionID, Track},
 };
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use voice_event::VoiceEvent;
 
 pub struct NoteTrack {
@@ -17,7 +17,13 @@ pub struct NoteTrack {
 
     // --- NOTE DATA ---
     regions: HashMap<RegionID, NoteRegion>,
+
+    // --- VOICE MANAGEMENT ---
     events: Vec<VoiceEvent>,
+    event_cursor: usize,
+    active_voices: VecDeque<usize>,
+    free_voices: Vec<usize>,
+    voice_buffer: Vec<Voice>,
 
     // --- AUDIO CONTEXT ---
     audio_ctx: AudioContext,
@@ -45,6 +51,19 @@ impl NoteTrack {
     fn add_region(&mut self, region: NoteRegion) {
         let id = self.generate_region_id();
         self.regions.insert(id, region);
+    }
+
+    // --- VOICE GETTING ---
+
+    /// Returns the vacant voice index, or returns the index of the oldest voice.
+    fn find_or_steal_voice(&mut self) -> usize {
+        let new_voice = self
+            .free_voices
+            .pop()
+            .or(self.active_voices.pop_front())
+            .unwrap_or_default();
+        self.active_voices.push_back(new_voice);
+        new_voice
     }
 }
 
@@ -90,12 +109,102 @@ impl Track for NoteTrack {
         // Sort the events
         self.events.sort_unstable_by_key(|e| e.sample_index);
 
+        // Initialize the voice buffer
+        self.voice_buffer = vec![
+            Voice::default();
+            self.audio_ctx.buffer_size as usize
+                * self.audio_ctx.max_voices as usize
+        ];
+
+        // Initialize the voices
+        self.active_voices.clear();
+        self.free_voices = (0..self.audio_ctx.max_voices as usize).collect();
+
         // Prepare the graph
         self.graph.prepare()
     }
 
     fn process(&mut self, playhead: Beats, output: *mut u8, audio_ctx: &AudioContext) {
+        // Convert the playhead beats to samples
+        let buffer_start = self.beats_to_samples(playhead);
+        let buffer_end = buffer_start + audio_ctx.buffer_size as usize;
+
+        let max_voices = audio_ctx.max_voices as usize;
+
+        // Seek the event cursor
+        if self
+            .events
+            .get(self.event_cursor)
+            .map_or(false, |e| e.sample_index > buffer_start)
+            || (self.event_cursor > 0
+                && self.events[self.event_cursor - 1].sample_index > buffer_start)
+        {
+            self.event_cursor = self
+                .events
+                .partition_point(|e| e.sample_index < buffer_start);
+        }
+
+        for sample in buffer_start..buffer_end {
+            // Calculate the local sample in the buffer chunk
+            let local_sample = sample - buffer_start;
+            // Calculate the index of the first current voice
+            let current = local_sample * max_voices;
+
+            // If the current sample is not the first sample,
+            // copy the previous voices to the current index
+            if local_sample > 0 {
+                let previous = (local_sample - 1) * max_voices;
+                // Get a mutable slice from the voice buffer
+                let (prev_slice, curr_slice) = self.voice_buffer.split_at_mut(current);
+                // Copy the previous slice to the mutable slice of the current buffer
+                curr_slice[..max_voices]
+                    .clone_from_slice(&prev_slice[previous..previous + max_voices]);
+
+                // Increment the elapsed_samples
+                for index in self.active_voices.iter() {
+                    self.voice_buffer[current + index].elapsed_samples += 1;
+                }
+            }
+
+            // Consume the events in this sample
+            while let Some(event) = self.events.get(self.event_cursor) {
+                // Break if the event's sample index is not current sample
+                if event.sample_index != sample {
+                    break;
+                }
+
+                // Copy the frequency and velocity to avoid reference issues
+                let frequency = event.frequency;
+                let velocity = event.velocity;
+
+                if event.is_note_on {
+                    // Start playing the note from the sample
+                    let voice_index = self.find_or_steal_voice();
+                    // Set the new voice to the voice buffer
+                    self.voice_buffer[current + voice_index] =
+                        Voice::new(frequency, velocity, true, 0);
+                } else {
+                    // Remove the active voice whose frequency matches the event frequency
+                    if let Some(remove_index) = self
+                        .active_voices
+                        .iter()
+                        .position(|&i| self.voice_buffer[current + i].frequency == event.frequency)
+                    {
+                        // Remove the index from the active_voices and get the voice index
+                        let voice_index = self.active_voices.remove(remove_index).unwrap();
+                        // Mark the voice index as free
+                        self.free_voices.push(voice_index);
+                        self.voice_buffer[current + voice_index].is_active = false;
+                    }
+                }
+                // Increment the event cursor
+                self.event_cursor += 1;
+            }
+        }
+
+        // Get a pointer to the voice buffer
+        let input_ptr = self.voice_buffer.as_ptr() as *const u8;
         // Process the graph
-        // self.graph.process(&[input_ptr], &[output], audio_ctx);
+        self.graph.process(&[input_ptr], &[output], audio_ctx);
     }
 }
