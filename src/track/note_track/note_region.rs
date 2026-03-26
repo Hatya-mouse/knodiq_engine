b6f823f62fe9841b84610ad0186107773023540d
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-03-26 19:41:03 +0900

    Add note modification functions to the note region

--- src/track/note_track/note_region.rs
+++ src/track/note_track/note_region.rs
@@ -14,6 +14,8 @@ pub struct NoteRegion {
 }
 
 impl NoteRegion {
+    // --- NEW ---
+
     /// Creates a new note region.
     pub fn new(start: Beats, duration: Beats) -> Self {
         Self {
@@ -24,12 +26,16 @@ impl NoteRegion {
         }
     }
 
+    // --- NOTE ID GENERATION ---
+
     /// Generates a new note ID.
     fn generate_note_id(&mut self) -> NoteID {
         let id = NoteID(self.next_note_id);
         self.next_note_id += 1;
         id
     }
+
+    // --- NOTE MANAGEMENT ---
 
     /// Adds a given note to the region.
     pub fn add_note(&mut self, note: Note) {
@@ -40,5 +46,42 @@ impl NoteRegion {
     /// Removes the note from the region.
     pub fn remove_note(&mut self, id: &NoteID) {
         self.notes.remove(id);
+    }
+
+    // --- NOTE GETTING ---
+
+    /// Returns a mutable reference to the note.
+    pub fn get_note_mut(&mut self, id: &NoteID) -> Option<&mut Note> {
+        self.notes.get_mut(id)
+    }
+
+    // --- NOTE MODIFICATION ---
+
+    /// Changes the note's start to the given start.
+    pub fn set_start(&mut self, id: &NoteID, start: Beats) {
+        if let Some(note) = self.get_note_mut(id) {
+            note.start = start;
+        }
+    }
+
+    /// Sets the note's duration to the given duration.
+    pub fn set_duration(&mut self, id: &NoteID, duration: Beats) {
+        if let Some(note) = self.get_note_mut(id) {
+            note.duration = duration;
+        }
+    }
+
+    /// Sets the note's pitch to the given pitch.
+    pub fn set_pitch(&mut self, id: &NoteID, pitch: f32) {
+        if let Some(note) = self.get_note_mut(id) {
+            note.pitch = pitch;
+        }
+    }
+
+    /// Sets the note's velocity to the given velocity.
+    pub fn set_velocity(&mut self, id: &NoteID, velocity: f32) {
+        if let Some(note) = self.get_note_mut(id) {
+            note.velocity = velocity;
+        }
     }
 }
