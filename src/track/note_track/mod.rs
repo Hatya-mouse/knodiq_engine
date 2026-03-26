42952930b6e13c302a7c0e7d61fee8815d1b0e55
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-03-26 11:21:58 +0900

    Remove event cursor seeking from process()

--- src/track/note_track/mod.rs
+++ src/track/note_track/mod.rs
@@ -190,14 +190,14 @@ impl Track for NoteTrack {
         let max_voices = self.audio_ctx.max_voices;
 
         // Seek the event cursor
-        if self
-            .events
-            .get(self.event_cursor)
-            .is_some_and(|e| e.sample_index > playhead)
-            || (self.event_cursor > 0 && self.events[self.event_cursor - 1].sample_index > playhead)
-        {
-            self.event_cursor = self.events.partition_point(|e| e.sample_index < playhead);
-        }
+        // if self
+        //     .events
+        //     .get(self.event_cursor)
+        //     .is_some_and(|e| e.sample_index > playhead)
+        //     || (self.event_cursor > 0 && self.events[self.event_cursor - 1].sample_index > playhead)
+        // {
+        //     self.event_cursor = self.events.partition_point(|e| e.sample_index < playhead);
+        // }
 
         for sample in playhead..buffer_end {
             // Calculate the local sample in the buffer chunk
